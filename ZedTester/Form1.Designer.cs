f474fa084e08b587feeacec0c87a6e025e35660d
miaodx <miaodx@hotmail.com>
2018-06-23 17:12:19 +0800

    Add ZedTester into the repo

---
+++ ZedTester/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ZedTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.versionInfo = new System.Windows.Forms.Label();
+            this.errorInfo = new System.Windows.Forms.Label();
+            this.sourceSelection = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // versionInfo
+            // 
+            this.versionInfo.AutoSize = true;
+            this.versionInfo.Location = new System.Drawing.Point(12, 8);
+            this.versionInfo.Name = "versionInfo";
+            this.versionInfo.Size = new System.Drawing.Size(71, 12);
+            this.versionInfo.TabIndex = 1;
+            this.versionInfo.Text = "VersionInfo";
+            // 
+            // errorInfo
+            // 
+            this.errorInfo.AutoSize = true;
+            this.errorInfo.Location = new System.Drawing.Point(12, 29);
+            this.errorInfo.Name = "errorInfo";
+            this.errorInfo.Size = new System.Drawing.Size(53, 12);
+            this.errorInfo.TabIndex = 2;
+            this.errorInfo.Text = "No Error";
+            // 
+            // sourceSelection
+            // 
+            this.sourceSelection.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.sourceSelection.FormattingEnabled = true;
+            this.sourceSelection.Items.AddRange(new object[] {
+            "Left",
+            "Right",
+            "Stereo",
+            "Depth",
+            "Confidence",
+            "Normals",
+            "Unrectified"});
+            this.sourceSelection.Location = new System.Drawing.Point(313, 11);
+            this.sourceSelection.Name = "sourceSelection";
+            this.sourceSelection.Size = new System.Drawing.Size(212, 20);
+            this.sourceSelection.TabIndex = 4;
+            this.sourceSelection.SelectedIndexChanged += new System.EventHandler(this.sourceSelection_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(263, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Source:";
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.imageBox1.Location = new System.Drawing.Point(24, 43);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(962, 558);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(685, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "CameraParams";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 613);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sourceSelection);
+            this.Controls.Add(this.errorInfo);
+            this.Controls.Add(this.versionInfo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label versionInfo;
+        private System.Windows.Forms.Label errorInfo;
+        private System.Windows.Forms.ComboBox sourceSelection;
+        private System.Windows.Forms.Label label1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ ZedTester/Form1.cs
@@ -0,0 +1,121 @@
+﻿// Form1.cs
+using sl;
+using System;
+using System.Windows.Forms;
+using ZED_Emgu.Cameras;
+using System.Diagnostics;
+using CameraRelocationSystem.Device;
+
+namespace ZedTester
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        VIEW view;
+        Zed zed = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // find version
+            var versionZED = "[SDK]: " + sl.ZEDCamera.GetSDKVersion().ToString() + " [Plugin]: " + sl.ZEDCamera.PluginVersion.ToString();
+            this.versionInfo.Text = versionZED;
+
+            this.Start();
+
+            Console.WriteLine("HOPE HERE!!");
+
+            /*
+            while (true)
+            { }
+             */ 
+        }
+
+        void Start()
+        {
+            // start with stored SVO
+            //var zed = new Zed(@"c:\Projects\files\HD720_SN17600_11-04-52.svo");
+
+            // start with live feed
+            zed = new Zed();
+
+            // You need to assign the listener that will execute in each camera loop
+            // in this case I named it AcquireImages and implemented it below
+            zed.FrameTick += this.AcquireImages;
+            zed.RuntimeParameters = new RuntimeParameters
+            {
+                sensingMode = SENSING_MODE.FILL,
+                enableDepth = true
+            };
+            //zed.Start();
+            //zed.Start_Sequential();
+            zed.Start_with_Thread();
+        }
+
+        private void AcquireImages(Zed zed)
+        {
+            try
+            {
+                imageBox1.Invoke((MethodInvoker)delegate
+                {
+                    // you can use method FetchImage or any of the helper methods
+                    // e.g. zed.Left, zed.Right, zed.Depth ...
+                    var image = zed.FetchImage(view);
+                    imageBox1.Image = image;
+                });
+            }
+            catch { }
+
+        }
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Is: " + ZEDCamera.CheckPlugin());
+        }
+
+
+        private void sourceSelection_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (sourceSelection.SelectedIndex)
+            {
+                case 0:
+                    view = sl.VIEW.LEFT;
+                    break;
+                case 1:
+                    view = sl.VIEW.RIGHT;
+                    break;
+                case 2:
+                    view = sl.VIEW.SIDE_BY_SIDE;
+                    break;
+                case 3:
+                    view = sl.VIEW.DEPTH;
+                    break;
+                case 4:
+                    view = sl.VIEW.CONFIDENCE;
+                    break;
+                case 5:
+                    view = sl.VIEW.NORMALS;
+                    break;
+                case 6:
+                    view = sl.VIEW.LEFT_UNRECTIFIED;
+                    break;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            ZEDCameraSettings cameraSetting = new ZEDCameraSettings();
+            cameraSetting.Connect(zed);
+            cameraSetting.showWindows();
+        }
+
+
+    }
+}
